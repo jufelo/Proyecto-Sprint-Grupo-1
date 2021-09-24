d6eb99d9c0058634d0eb9ad3678530b5751bb6b0
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-09-24 14:51:54 -0500

    Avance Sprint 2

--- form.js
+++ form.js
@@ -1,19 +1,25 @@
-function checkNombre(valor) {
-    
-}
+var nombre = document.getElementById("nombre");
+var apellido = document.getElementById("apellido");
+error.style.color = "red";
 
-function checkApellido(valor) {
+var form = document.getElementById("form-registro");
 
-}
+    form.addEventListener("submit", function(evt){
+        evt.preventDefault();
+        //console.log("Enviando formulario...");
+        //alert("Prueba");
+        var mensajesError = [];
 
-function checkTelefono(valor) {
 
-}
 
-function checkCorreo(valor) {
+        if(nombre.value === null || nombre.value === ""){
+            mensajesError.push("Ingresa tu nombre");
+        }
+        if(apellido.value === null || apellido.value === ""){
+            mensajesError.push("Ingresa tu apellido");
+        }
+        error.innerHTML = mensajesError.join(', ');
 
-}
+});
 
-function checkContrasena(valor) {
 
-}
