68c125c88b9c6a42c8d88a5d96e34e7557f7f436
dianagarcial <diana_ma.garcia@uao.edu.co>
2021-09-25 16:17:01 -0500

    intento form.js

--- form.js
+++ form.js
@@ -1,28 +1,104 @@
-export default module.exports= checkNombre;
-var valoresAceptadosNumeros = /^[0-9]+$/;
-var valoresAceptadosLetras=/^[a-zA-Z]+$/;
-function checkNombre(valor) {
-    if(valor.match(valoresAceptadosLetras)){
-        if (valor.value.length > 4 && valor.value.length < 30 ) {
-            return true;
-            }	 
-        else {
-            return false;
-        }
+export default checkNombre;
+export default checkApellido;
+export default checkTelefono;
+export default checkCorreo;
+export default checkContrasena;
 
-    }else{
-        alert('Digite solo caracteres alfabeticos')
-        return false;
+function click_boton()
+{
+    let name = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    checkNombre(nombre);
+    checkApellido(apellido);
+    checkTelefono(telefono, "telefono");
+
+}
+function checkNombre(valor) {
+    let verificacion = validar_texto(valor);let array_campo = Array.from(valor);
+    let verificacion = true;
+    if (campo != "") {
+        if (array_campo.length <= 30 && array_campo.length >= 4) {
+            for(let i=0; i<array_campo.length; i++)
+            {
+                if(array_campo[i] == "0" || array_campo[i] == "1" || array_campo[i] == "2" || array_campo[i] == "3" || array_campo[i] == "4" || array_campo[i] == "5" || array_campo[i] == "6" || array_campo[i] == "7" || array_campo[i] == "8" || array_campo[i] == "9"){  
+                    verificacion = false;
+                    i = array_campo.length;
+                                    
+                }
+            }
+        }
+        else
+        {
+            verificacion = false;
+            
+        }       
     }
+    else
+    {
+        verificacion = false;   
+        
+    }
+    return verificacion;
+    
     
          
 }
 
 function checkApellido(valor) {
+    let array_campo = Array.from(valor);
+    let verificacion = true;
+    if (campo != "") {
+        if (array_campo.length <= 30 && array_campo.length >= 4) {
+            for(let i=0; i<array_campo.length; i++)
+            {
+                if(array_campo[i] == "0" || array_campo[i] == "1" || array_campo[i] == "2" || array_campo[i] == "3" || array_campo[i] == "4" || array_campo[i] == "5" || array_campo[i] == "6" || array_campo[i] == "7" || array_campo[i] == "8" || array_campo[i] == "9"){  
+                    verificacion = false;
+                    i = array_campo.length;                  
+                }
+            }
+        }
+        else
+        {
+            verificacion = false;
+        }       
+    }
+    else
+    {
+        verificacion = false;       
+    }
+    return verificacion;
 
 }
 
 function checkTelefono(valor) {
+    let array_campo = Array.from(valor);
+    let verificacion = true;
+    if(campo != ""){
+        if(array_campo.length == 7){
+            for(let i=0; i<array_campo.length; i++)
+            {                
+                if(array_campo[i] == "0" || array_campo[i] == "1" || array_campo[i] == "2" || array_campo[i] == "3" || array_campo[i] == "4" || array_campo[i] == "5" || array_campo[i] == "6" || array_campo[i] == "7" || array_campo[i] == "8" || array_campo[i] == "9"){ 
+                                                        
+                }
+                else
+                {
+                    verificacion = false;
+                    i = array_campo.length;  
+                }
+            }
+        }
+        else
+        {
+            verificacion = false;
+        }
+
+    }
+    else
+    {
+        verificacion = false;
+    }
+    return verificacion;
 
 }
 
@@ -32,4 +108,9 @@ function checkCorreo(valor) {
 
 function checkContrasena(valor) {
 
+
 }
+
+
+
+
