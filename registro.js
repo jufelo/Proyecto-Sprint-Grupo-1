82a9d9ca9cf20bf31cd07f6ff7cd591aff8bcf82
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-09-27 17:41:59 -0500

    Create registro.js
    Se implementa funciones y exportaciones

---
+++ registro.js
@@ -0,0 +1,15 @@
+function agregarRegistro() {
+    
+}
+
+function  ordenarArreglo(){
+
+}
+
+function filtrarCorreo(){
+
+}
+
+module.exports = agregarRegistro;
+module.exports = ordenarArreglo;
+module.exports = filtrarCorreo;
