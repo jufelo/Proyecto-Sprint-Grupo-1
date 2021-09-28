5f914545a2486fca8797d796405692993a251beb
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-09-27 20:59:55 -0500

    Update registro.js
    Se agrega return arreglo, linea 4.
    Se comenta linea 15 al 17 (sentencias redundantes).
    Pendiente subir al calificador, rubrica  no está todavía habilitada.

--- registro.js
+++ registro.js
@@ -1,6 +1,7 @@
 function agregarRegistro() {
     arreglo = {nombre: document.getElementById("nombre").value, apellido: document.getElementById("apellido").value, telefono: document.getElementById("telefono"), correo: document.getElementById("correo")};
     arreglo.push(arreglo);
+    return arreglo;
 }
 
 function  ordenarArreglo(){
@@ -10,7 +11,10 @@ function  ordenarArreglo(){
 function filtrarCorreo(){
 
 }
-
+/*
 module.exports = agregarRegistro;
 module.exports = ordenarArreglo;
 module.exports = filtrarCorreo;
+*/
+
+module.exports = {agregarRegistro, ordenarArreglo, filtrarCorreo};
