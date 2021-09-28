a9bb415b9d2390c3fcb01635f43572bcd4e304dc
JUAN FERNANDO LOPEZ OROZCO <juanflopez@hotmail.com>
2021-09-27 19:57:45 -0500

    Update registro.js
    Desarrollo de la función agregarRegistro ()

--- registro.js
+++ registro.js
@@ -1,5 +1,6 @@
 function agregarRegistro() {
-    
+    arreglo = {nombre: document.getElementById("nombre").value, apellido: document.getElementById("apellido").value, telefono: document.getElementById("telefono"), correo: document.getElementById("correo")};
+    arreglo.push(arreglo);
 }
 
 function  ordenarArreglo(){
