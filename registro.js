38fb6996806cb2637e5fb0c67515c70d3fd31a3c
Arana <77128067+aaranaa5@users.noreply.github.com>
2021-10-01 19:46:24 -0500

    Add files via upload

--- registro.js
+++ registro.js
@@ -1,51 +1,47 @@
 class usuario{
-  constructor(nombre, apellido, telefono, correo, contrasena){
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.telefono = telefono;
-      this.correo = correo;
-      this.contrasena = contrasena;
-  }
+    constructor(nombre,apellido,telefono,correo,contrasena){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.telefono=telefono;
+        this.correo=correo;
+        this.contrasena=contrasena;
+    }
 
 }
 
 var registros=[];
 
 function agregarRegistro(){
-  nombre = document.getElementById("nombre").value;
-  apellido = document.getElementById("apellido").value;
-  telefono = document.getElementById("telefono").value;
-  correo = document.getElementById("correo").value;
-  contrasena = document.getElementById("contrasena").value;
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+    telefono = document.getElementById("telefono").value;
+    correo = document.getElementById("correo").value;
+    contrasena = document.getElementById("contrasena").value;
 
-  let registro= new usuario(nombre, apellido, telefono, correo, contrasena);
+    let registro= new usuario(nombre,apellido,telefono,correo,contrasena);
+    
+    registros.push(registro); 
 
-  registros.push(registro);
-  console.log(registro);
-
-  return registros;
+    return registros;
 
 }
 
 function ordenarArreglo(arreglo){
 
-  arreglo.sort((a,b) => {
-      if(a.apellido.toLowerCase() == b.apellido.toLowerCase()) {return 0;}
-      else if (a.apellido.toLowerCase() > b.apellido.toLowerCase()) {return 1;}
-      else {return -1;}
-  });
-  console.table(arreglo);
-  return arreglo;
+    arreglo.sort((a,b) => {
+        if(a.apellido.toLowerCase() == b.apellido.toLowerCase())      {return 0;}
+        else if (a.apellido.toLowerCase() > b.apellido.toLowerCase()) {return 1;}
+        else {return -1;}
+    } )
+
+    return arreglo;
 }
 
 function filtrarCorreo(arreglo){
 
-  filtrado = arreglo.filter(function (email) {
-      return email.correo.endsWith('gmail.com');
-  });
-  console.table(filtrado);
+    let filtrado = arreglo.filter(filtro => filtro.correo.endsWith('@gmail.com'));
 
-  return filtrado;
+    return filtrado;
 }
 
 module.exports.registros = registros;
